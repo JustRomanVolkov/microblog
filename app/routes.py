f1c4631cbc04d0827e6af0da690b8144e50471b5
R2 <volkovr_vk@mail.ru>
2023-10-10 11:40:01 +0300

    add for block jinja to posts in index.html change routes.py

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,19 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Роман'}
-    return render_template('index.html', title='Главная страница', user=user)
-
+    user = {'username': 'Роман Волков'}
+    posts = [
+        {
+            'author': {'username': 'Олег'},
+            'body': 'Всем привет!'
+        },
+        {
+            'author': {'username': 'Маша'},
+            'body': 'Всем чмоки в этом чатике'
+        },
+        {
+            'author': {'username': 'Петя'},
+            'body': 'Дароу!'
+        }
+    ]
+    return render_template('index.html', title='Главная страница', user=user, posts=posts)
