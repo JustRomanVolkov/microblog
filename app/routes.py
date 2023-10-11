71c715c2edeabf3b808943a4864086722b57be41
R2 <volkovr_vk@mail.ru>
2023-10-11 11:17:22 +0300

    add login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -23,3 +24,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Главная страница', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Логин запрошен для пользователя {form.username.data},'
+              f'запомни меня = {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Авторизация', form=form)
