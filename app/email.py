6821de22ce107ada8f2b712827b696a255353605
R2 <volkovr_vk@mail.ru>
2023-10-19 02:12:15 +0700

    add async send email

--- app/email.py
+++ app/email.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 
 # Стандартные библиотеки Python
+from threading import Thread
 
 # Библиотеки третьей стороны
-from flask import render_template
+from flask import Flask, render_template
 from flask_mail import Message
-
 
 # Собственные модули
 from app import app, mail
 
 
+def send_async_email(app: Flask, msg: Message) -> None:
+    """
+    Функция для асинхронной отправки электронной почты.
+
+    Args:
+        app: Объект Flask-приложения.
+        msg (Message): Объект сообщения для отправки.
+
+    """
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(subject: str, sender: str, recipients: list, text_body: str, html_body: str) -> None:
     """
-    Отправляет электронное письмо.
+    Отправляет электронное письмо асинхронно.
 
     Args:
         subject (str): Тема письма.
@@ -28,8 +41,7 @@ def send_email(subject: str, sender: str, recipients: list, text_body: str, html
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    # Предполагается, что у вас есть объект mail для отправки писем
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user: 'User') -> None:
@@ -44,7 +56,7 @@ def send_password_reset_email(user: 'User') -> None:
 
     """
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
+    send_email('[Микроблог] Сброс Вашего Пароля.',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt',
