69787425b7e5353fde7d43906d89a7469d2c6c92
R2 <volkovr_vk@mail.ru>
2023-10-28 20:35:09 +0700

    fix automatic AJAX translation for posts on 'Translate' link click

--- app/static/js/translate.js
+++ app/static/js/translate.js
@@ -1,48 +1,80 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Найти все элементы с классом "translate-link"
-    const translateLinks = document.querySelectorAll('.translate-link');
+    // Задать исходный и целевой языки
+    const sourceLanguage = "auto"; // Автоопределение исходного языка
+    const targetLanguage = "ru"; // Замените на фактический язык перевода
 
-    // Пройти по каждой ссылке и добавить обработчик события "click"
-    translateLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            // Остановить действие по умолчанию (предотвратить переход по ссылке)
+    // Создаем объект, который будет хранить текущее состояние каждого поста
+    const postStates = {};
+
+    // Создаем объект, который будет хранить количество нажатий на каждую ссылку "Translate"
+    const clickCount = {};
+
+    // Делегирование событий на родительском элементе, который существует на момент загрузки страницы
+    document.body.addEventListener('click', function(event) {
+        const target = event.target;
+
+        // Проверить, был ли клик на элементе с классом "translate-link"
+        if (target.classList.contains('translate-link')) {
             event.preventDefault();
-            translatePost(link);
-        });
+
+            // Найти соответствующий элемент с текстом поста
+            const postId = target.id;
+            const postTextElement = document.querySelector(`.post-text[data-postid="${postId}"]`);
+
+            // Проверить, что элемент с текстом существует
+            if (postTextElement) {
+                // Переключить между оригинальным текстом и переводом
+                toggleTranslation(postTextElement, target, sourceLanguage, targetLanguage, postStates, clickCount);
+            }
+        }
     });
 
-    // Функция для выполнения перевода поста
-    function translatePost(link) {
-        // Получить уникальный идентификатор поста из ID ссылки
-        const postId = link.id;
+    // Функция для переключения между оригинальным текстом и переводом
+    function toggleTranslation(postTextElement, link, sourceLang, targetLang, postStates, clickCount) {
+        const postId = postTextElement.dataset.postid;
+        const currentState = postStates[postId] || "original";
 
-        // Найти соответствующий элемент с текстом поста
-        const postTextElement = document.querySelector(`.post-text[data-postid="${postId}"]`);
-
-        // Проверить, что элемент с текстом существует
-        if (postTextElement) {
-            // Получить текст поста
-            const postText = postTextElement.textContent;
-
-            // Задать исходный и целевой языки
-            const sourceLanguage = "auto"; // Автоопределение исходного языка
-            const targetLanguage = "ru"; // Замените на фактический язык перевода
-
-            // Сформировать URL-запрос для Google Translate API
-            const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(postText)}`;
-
-            // Выполнить запрос к Google Translate API
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success: function (data) {
-                    // Извлечь перевод из ответа
-                    var translation = data[0][0][0];
-
-                    // Вставить перевод в элемент с текстом поста
-                    postTextElement.textContent = translation;
-                }
-            });
+        if (!clickCount[postId]) {
+            clickCount[postId] = 0;
         }
+        clickCount[postId]++;
+
+        if (clickCount[postId] % 2 === 1) {
+            // Нечетное нажатие, переключиться на перевод
+            translateText(postTextElement, sourceLang, targetLang, link, postStates);
+        } else {
+            // Четное нажатие, переключиться на оригинал
+            if (currentState === "original") {
+                // Текущее состояние - оригинальный текст, ничего не меняем
+                return;
+            }
+            postTextElement.textContent = postStates[postId].originalText;
+            link.textContent = 'Translate'; // Измените текст ссылки на "Translate"
+            postStates[postId] = { state: "original", originalText: postStates[postId].originalText };
+        }
+    }
+
+    // Функция для выполнения перевода текста
+    function translateText(postTextElement, sourceLang, targetLang, link, postStates) {
+        const originalText = postTextElement.textContent;
+        const postId = postTextElement.dataset.postid;
+
+        const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURIComponent(originalText)}`;
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function (data) {
+                var translation = data[0][0][0];
+                postTextElement.textContent = translation;
+                link.textContent = 'Original'; // Измените текст ссылки на "Original"
+
+                // Сохраняем состояние поста
+                postStates[postId] = { state: "translation", originalText: originalText };
+            },
+            error: function() {
+                console.log("Translation request failed");
+            }
+        });
     }
 });
