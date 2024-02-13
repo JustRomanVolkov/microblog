35c2d49a800de024bbe1a16a0a2c53826a64abbe
Roman Volkov <101933459+TheRomanVolkov@users.noreply.github.com>
2024-02-13 18:15:28 +0700

    Update review_script.py

--- .github/scripts/review_script.py
+++ .github/scripts/review_script.py
@@ -25,7 +25,7 @@ def send_request_to_openai(prompt):
     }
     
     max_retries = 5
-    retry_delay = 60  # Время задержки в секундах
+    retry_delay = 30  # Время задержки в секундах
     
     
     # Формируем тело запроса с параметрами для модели OpenAI.
@@ -53,6 +53,8 @@ def post_comment_to_pr(comment_body, pull_request_number):
         'Accept': 'application/vnd.github.v3+json',
     }
     # Устанавливаем заголовки запроса, включая токен авторизации GitHub.
+    print(f"GITHUB_REPOSITORY: {os.getenv('GITHUB_REPOSITORY')}")
+
 
     pr_comment_url = f'https://api.github.com/repos/{os.getenv("GITHUB_REPOSITORY")}/issues/{pull_request_number}/comments'
     # Формируем URL для создания комментария на GitHub.
@@ -67,7 +69,9 @@ if __name__ == "__main__":
     # Получаем данные события из файла события, который предоставляет GitHub
     with open(os.getenv('GITHUB_EVENT_PATH')) as event_file:
         event_data = json.load(event_file)
-    pull_request_number = event_data['number']  # Получаем номер пулл-реквеста из данных события
+        print(json.dumps(event_data, indent=4))  # Для наглядности структуры JSON
+
+    pull_request_number = event_data['pull_request']['number'] # Получаем номер пулл-реквеста из данных события
 
     # Проверяем, является ли файл исполняемым, и запускаем основную логику скрипта.
     prompt = os.getenv('PROMPT', 'Please review the following code for any issues or suggestions for improvements:')
